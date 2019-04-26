d5a22d1f307b2ccd1f69623e14107c11b6cdc6e8
Harry Ho <thienhungho@users.noreply.github.com>
2019-04-26 16:18:58 +0700

    Add login form

---
+++ src/models/LoginForm.php
@@ -0,0 +1,94 @@
+<?php
+namespace thienhungho\UserManagement\models;
+
+use \thienhungho\UserManagement\models\User;
+use Yii;
+use yii\base\Model;
+
+/**
+ * Login form
+ */
+class LoginForm extends Model
+{
+    public $username;
+    public $password;
+    public $rememberMe = true;
+
+    private $_user;
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            // username and password are both required
+            [['username', 'password'], 'required'],
+            // rememberMe must be a boolean value
+            ['rememberMe', 'boolean'],
+            // password is validated by validatePassword()
+            ['password', 'validatePassword'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'username' => Yii::t('app', 'Username'),
+            'rememberMe' => Yii::t('app', 'RememberMe'),
+            'password' => Yii::t('app', 'Password'),
+            'email' => Yii::t('app', 'Email'),
+        ];
+    }
+
+    /**
+     * Validates the password.
+     * This method serves as the inline validation for password.
+     *
+     * @param string $attribute the attribute currently being validated
+     * @param array $params the additional name-value pairs given in the rule
+     */
+    public function validatePassword($attribute, $params)
+    {
+        if (!$this->hasErrors()) {
+            $user = $this->getUser();
+            if (!$user || !$user->validatePassword($this->password)) {
+                $this->addError($attribute, Yii::t( 'app', 'Incorrect username or password.'));
+            }
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    public function login()
+    {
+        if ($this->validate()) {
+            return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600 * 24 * 30 : 0);
+        }
+        
+        return false;
+    }
+
+    /**
+     * Finds user by [[username]]
+     *
+     * @return User|null
+     */
+    protected function getUser()
+    {
+        if ($this->_user === null) {
+            $user = User::findByUsername($this->username);
+            if (is_role(['admin', 'manager', 'dev'], $user)) {
+                $this->_user = $user;
+            }
+        }
+
+        return $this->_user;
+    }
+}
